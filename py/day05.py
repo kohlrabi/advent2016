e9ebd13bce39567e83aad420de86c6b1ede6390c
Christoph Terasa <christoph@kohlio.de>
2016-12-05 18:11:49 +0100

    py/day05.py

--- py/day05.py
+++ py/day05.py
@@ -11,10 +11,25 @@ def part1(inp):
         i += 1
         if h[:5] == '00000':
             ret += h[5]
-            if len(ret) == 8:
-                break
 
     return ret
 
+def part2(inp):
+    import hashlib
+
+    ret = ['x']*8
+    i = 0
+    while(ret.count('x') > 0):
+        h = hashlib.md5(inp + str(i)).hexdigest()
+        i += 1
+        if h[:5] == '00000':
+            pos = int(h[5],16)
+            if pos < 8 and ret[pos] == 'x':
+                ret[pos] = h[6]
+
+    return "".join(ret)
+
+
 if __name__ == '__main__':
     print "part1:", part1(inp)
+    print "part2:", part2(inp)
