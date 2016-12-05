279a5aa505ae8fbaee5ec9fba7df3dc8444bc8eb
Christoph Terasa <christoph@kohlio.de>
2016-12-05 18:56:54 +0100

    py/day04.py: Finished part 1.

--- py/day04.py
+++ py/day04.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import fileinput
 
 s = [l for l in fileinput.input()]
@@ -5,11 +7,11 @@ s = [l for l in fileinput.input()]
 def part1(s):
     import collections
     import itertools
+    import operator
 
     ret = 0
     
     for l in s:
-        ok = True
         d = collections.defaultdict(lambda: 0)
         sl = l.split('-')
         for ssl in sl[:-1]:
@@ -17,21 +19,16 @@ def part1(s):
                 d[c] += 1
         check = sl[-1][-7:-2]
         idd = int(sl[-1].split('[')[0])
-        dd = [d[c] for c in check]
-        
-        for i in sorted(d.values())[::-1]:
-            if 
-            if dd[i] < dd[i+1]:
-                ok = False
-            elif dd[i] == dd[i+1] and ord(check[i]) > ord(check[i+1]):
-                    ok = False
 
-        ret += idd * ok
+        ss = sorted(d.items(), key=lambda x: (x[1], -ord(x[0])), reverse=True)
+        r = "".join([sss[0] for sss in ss[:5]])
+
+        if r == check:
+            ret += idd 
 
     return ret
 
-
-print "part 1", part1(s)
+print "part 1:", part1(s)
 
 
 
