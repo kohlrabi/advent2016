60278d8e6b09c90662166c127f70219d7a8e66d8
Christoph Terasa <christoph@kohlio.de>
2016-12-06 13:09:31 +0100

    py/day06.py: Refactored.

--- py/day06.py
+++ py/day06.py
@@ -5,17 +5,14 @@ import collections
 
 s = [l for l in fileinput.input()]
 
-def part1(s):
+def part1(s, index=0):
     t = zip(*s)[:-1]
-    r = [collections.Counter(tt).most_common(1)[0] for tt in t]
+    r = [collections.Counter(tt).most_common()[index] for tt in t]
     ret = "".join([rr[0] for rr in r])
     return ret
 
 def part2(s):
-    t = zip(*s)[:-1]
-    r = [collections.Counter(tt).most_common()[-1] for tt in t]
-    ret = "".join([rr[0] for rr in r])
-    return ret
+    return part1(s, index=-1)
 
 print "part 1:", part1(s)
 print "part 2:", part2(s)
