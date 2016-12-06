387b61a95a68d6e44f3ac400f51c102681a11c0d
Christoph Terasa <christoph@kohlio.de>
2016-12-06 11:07:26 +0100

    py/day06.py: part2.

--- py/day06.py
+++ py/day06.py
@@ -11,5 +11,12 @@ def part1(s):
     ret = "".join([rr[0] for rr in r])
     return ret
 
+def part2(s):
+    t = zip(*s)[:-1]
+    r = [collections.Counter(tt).most_common()[-1] for tt in t]
+    ret = "".join([rr[0] for rr in r])
+    return ret
+
 print "part 1:", part1(s)
+print "part 2:", part2(s)
 
