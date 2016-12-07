78c2e7c0db6bc7a1c3730ff7a64aad1aa07530fb
Christoph Terasa <christoph@kohlio.de>
2016-12-07 14:50:44 +0100

    py/day07.py

--- py/day07.py
+++ py/day07.py
@@ -22,8 +22,37 @@ def part1(s):
 
     return n
 
+def checkline2(l):
+    import re
+    matches = []
+    for c in zip(l[0:-2], l[1:-1], l[2:]):
+        if c[0] == c[2]:
+            matches.append("".join((c[1], c[0], c[1])))
+    return matches
+
+def part2(s):
+    import re
+
+    n = 0
+    for l in s:
+        ll = re.split(r"[\[\]]", l)
+
+        gl = [checkline2(lll) for lll in ll[::2]]
+
+        
+        found = False
+        for gll in gl:
+            for m in gll:
+                for bl in ll[1::2]:
+                    if m in bl and not found:
+                        n += 1
+                        found = True
+    
+    return n
+
 if __name__ == '__main__':
     import fileinput
 
     s = [l for l in fileinput.input()]
     print "part1:", part1(s)
+    print "part2:", part2(s)
